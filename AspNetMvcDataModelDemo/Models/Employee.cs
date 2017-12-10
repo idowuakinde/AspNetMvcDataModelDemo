56abdcedd318921af82387f4b819554be505bf72
idowuakinde <8018104+idowuakinde@users.noreply.github.com>
2017-12-10 01:30:37 +0100

    Initial check-in of module AspNetMvcDataModelDemo

---
+++ AspNetMvcDataModelDemo/Models/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMvcDataModelDemo.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            // Default Constructor. Do nothing
+        }
+        public Employee(int ID, string Name, DateTime JoiningDate, int Age)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.JoiningDate = JoiningDate;
+            this.Age = Age;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext()
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
